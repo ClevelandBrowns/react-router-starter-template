5f25dda6191ea872fd8b2c4c98cb3a3b97170ce8
J <developingwilburys@outlook.com>
2026-08-15 15:39:51 -0500

    changed title and description for the webpage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
 	return [
-		{ title: "New React Router App" },
-		{ name: "description", content: "Welcome to React Router!" },
+		{ title: "UMKC Catholic" },
+		{ name: "description", content: "The official website of the Catholic community at the University of Missouri-Kansas City!" },
 	];
 }
 
